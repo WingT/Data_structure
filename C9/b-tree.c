683f5caf432fd3689307cbe94ef096c667596b22
wingt <13974454240@qq.com>
2016-11-22 16:09:56 +0800

    16.11.22

--- C9/b-tree.c
+++ C9/b-tree.c
@@ -12,6 +12,21 @@ typedef struct result{
 }result;
 int m=5;
 int min=2,max=4;
+void bm_print(node_t *t){
+	if (t==NULL)
+		return;
+	printf("[");
+	for (int i=1;i<t->n;i++)
+		printf("%d,",t->key[i]);
+	printf("%d]",t->key[t->n]);
+	printf("{");
+	for (int i=0;i<t->n;i++){
+		bm_print(t->c[i]);
+                printf(",");
+        }
+	bm_print(t->c[t->n]);
+	printf("}");
+}
 int search_in_node(node_t *t,int key){
 	int i;
 	for (i=0;i<t->n && key>=t->key[i+1];i++);
@@ -22,13 +37,13 @@ result bm_search(node_t *t,int key){
 	result r;
 	if (t==NULL){
 		r.flag=0;
-		r.i=0;
+		r.i=-1;
 		r.p=NULL;
 	}
 	int i;
 	while (p){
-		i=search_in_node(t,key);
-		if (i>0 && t->key[i]==key){
+		i=search_in_node(p,key);
+		if (i>0 && p->key[i]==key){
 			r.flag=1;
 			r.i=i;
 			r.p=p;
@@ -64,8 +79,8 @@ int bm_insert(node_t **t,int key){
 		return 0;
 	pt=r.p;
 	int tmp_key[MAXM];
-    node_t *tmp_c[MAXM+1];
-	node_t *newc=NULL;
+        node_t *tmp_c[MAXM+1];
+        node_t *newc=NULL;
 	while (1){
 		int k=search_in_node(pt,key);
 		if (pt->n==max){
@@ -127,12 +142,13 @@ int bm_delete(node_t **t,int key){
 		return 0;
 	pt=r.p;
 	if (pt->c[r.i]!=NULL){
-		node_t **succ=&pt->c[r.i];
-		while ((*succ)->c[0])
-			succ=&(*succ)->c[0];
-		pt->key[r.i]=(*succ)->key[0];
-		bm_delete(succ,pt->key[r.i]);
-		return 1;
+		node_t *succ=pt->c[r.i];
+		while (succ->c[0])
+			succ=succ->c[0];
+		pt->key[r.i]=succ->key[1];
+                key=pt->key[r.i];
+                r=bm_search(succ,key);
+                pt=r.p;
 	}
 	node_t *p=pt->p;
 	if (p==NULL){
@@ -146,36 +162,9 @@ int bm_delete(node_t **t,int key){
 		pt->n--;
 		return 1;
 	}
-	if (pt->n>min){
-		for (int i=r.i;i<pt->n;i++)
-			pt->key[i]=pt->key[i+1];
-		pt->n--;
-		return 1;
-	}
-	int k=search_in_node(p,key);
-	node_t *lb=k>0?p->c[k-1]:NULL;
-	node_t *rb=k<p->n?p->c[k+1]:NULL;
-	if (lb!=NULL && lb->n > min){
-		for (int i=r.i;i>0;i--)
-			pt->key[i]=pt->key[i-1];
-		pt->key[0]=p->key[k];
-		p->key[k]=lb->key[lb->n];
-		lb->n--;
-		return 1;
-	}
-	if (rb!=NULL && rb->n > min){
-		for (int i=r.i;i<pt->n;i++)
-			pt->key[i]=pt->key[i+1];
-		pt->key[pt->n]=p->key[k+1];
-		p->key[k+1]=rb->key[0];
-		for (int i=1;i<=rb->n;i++)
-			rb->key[i-1]=rb->key[i];
-		rb->n--;
-		return 1;
-	}
-	for (int i=r.i;i<pt->n;i++)
-		pt->key[i]=pt->key[i+1];
-	pt->n--;
+        for (int i=r.i;i<pt->n;i++)
+          pt->key[i]=pt->key[i+1];
+        pt->n--;
 	while (1){
 		if (pt->n>=min)
 			break;
@@ -187,71 +176,76 @@ int bm_delete(node_t **t,int key){
 			}
 			break;
 		}
-		k=search_in_node(p,pt->key[0]);
-		lb=k>0?p->c[k-1]:NULL;
-		rb=k<p->n?p->c[k+1]:NULL;
-		if (lb!=NULL){
-			lb->key[lb->n+1]=p->key[k];
-			for (int i=1;i<=pt->n;i++)
-				lb->key[lb->n+i+1]=pt->key[i];
-			for (int i=0;i<=pt->n;i++){
-				lb->c[lb->n+1+i]=pt->c[i];
-				if (pt->c[i])
-					pt->c[i]->p=lb;
-			}
-			lb->n+=pt->n+1;
-			free(pt);
-			for (int i=k;i<p->n;i++){
-				p->key[i]=p->key[i+1];
-				p->c[i]=p->c[i+1];
-			}
-			p->n--;
-		}
-		else{
-			pt->key[pt->n+1]=p->key[k+1];
-			for (int i=1;i<=rb->n;i++)
-				pt->key[pt->n+i+1]=rb->key[i];
-			for (int i=0;i<=rb->n;i++){
-				pt->c[pt->n+1+i]=rb->c[i];
-				if (rb->c[i])
-					rb->c[i]->p=pt;
-			}
-			pt->n+=rb->n+1;
-			free(rb);
-			for (int i=k+1;i<p->n;i++){
-				p->key[i]=p->key[i+1];
-				p->c[i]=p->c[i+1];
-			}
-			p->n--;
-		}
+		int k=search_in_node(p,key);
+		node_t *lb=k>0?p->c[k-1]:NULL;
+                node_t *rb=k<p->n?p->c[k+1]:NULL;
+                if (lb!=NULL && lb->n > min){
+                  for (int i=pt->n;i>0;i--)
+                    pt->key[i+1]=pt->key[i];
+                  pt->key[1]=p->key[k];
+                  for (int i=pt->n;i>=0;i--)
+                    pt->c[i+1]=pt->c[i];
+                  pt->c[0]=lb->c[lb->n];
+                  if (pt->c[0])
+                    pt->c[0]->p=pt;
+                  pt->n++;
+                  p->key[k]=lb->key[lb->n];
+                  lb->n--;
+                }
+                else{
+                  if (rb!=NULL && rb->n > min){
+                    pt->key[pt->n+1]=p->key[k+1];
+                    pt->c[pt->n+1]=rb->c[0];
+                    if (rb->c[0])
+                      rb->c[0]->p=pt;
+                    pt->n++;
+                    p->key[k+1]=rb->key[1];
+                    for (int i=1;i<rb->n;i++)
+                      rb->key[i]=rb->key[i+1];
+                    for (int i=0;i<rb->n;i++)
+                      rb->c[i]=rb->c[i+1];
+                    rb->n--;
+                  }
+                  else{
+                    if (lb==NULL){
+                      lb=pt;
+                      pt=rb;
+                      k++;
+                    }
+                    lb->key[lb->n+1]=p->key[k];
+                    for (int i=1;i<=pt->n;i++)
+                      lb->key[lb->n+i+1]=pt->key[i];
+                    for (int i=0;i<=pt->n;i++){
+                      lb->c[lb->n+1+i]=pt->c[i];
+                      if (pt->c[i])
+                        pt->c[i]->p=lb;
+                    }
+                    lb->n+=pt->n+1;
+                    free(pt);
+                    for (int i=k;i<p->n;i++){
+                      p->key[i]=p->key[i+1];
+                      p->c[i]=p->c[i+1];
+                    }
+                    p->n--;
+                  }
+                }
 		pt=p;
 		p=p->p;
 	}
 	return 1;
 }
-void bm_print(node_t *t){
-	if (t==NULL)
-		return;
-	printf("[");
-	for (int i=1;i<t->n;i++)
-		printf("%d,",t->key[i]);
-	printf("%d]",t->key[t->n]);
-	printf("{");
-	for (int i=0;i<t->n;i++){
-		printf("%d",i);
-		bm_print(t->c[i]);
-		printf(",");
-	}
-	printf("%d",t->n);
-	bm_print(t->c[t->n]);
-	printf("}");
-}
 int main(int argc,char **argv){
 	node_t *t=NULL;
-	for (int i=1;i<argc;i++){
-		int tmp=atoi(argv[i]);
+        int m=atoi(argv[1]);
+	for (int i=0;i<m;i++){
+		int tmp=atoi(argv[i+2]);
 		bm_insert(&t,tmp);
-		bm_print(t);
-		printf("\n");
 	}
+        int n=atoi(argv[m+2]);
+        for (int i=0;i<n;i++){
+          int tmp=atoi(argv[m+3+i]);
+          bm_delete(&t,tmp);
+          bm_print(t);
+          printf("\n");
+        }
 }
